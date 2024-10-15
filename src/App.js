5527d0733042092dab273d89f150d7f743f9d845
Santhi Raju <santhiraju32@gmail.com>
2024-10-15 21:51:45 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [pdfFile, setPdfFile] = useState(null);
+  const [pdfUrl, setPdfUrl] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+  const handleChangeFile = (e) =>{
+    const file = e.target.files[0];
+
+    if(file && file.type === 'application/pdf'){
+      const fileUrl = URL.createObjectURL(file);
+      setPdfFile(file);
+      setPdfUrl(fileUrl);
+    }
+    else{
+      alert('Upload a valid file..')
+    }
+  }
+
+
+  const handleViewPdf = ()=>{
+    if(pdfFile){
+      setIsModalOpen(true)
+    }
+    else{
+      alert('Upload PDF first...')
+    }
+  }
+
+
+  const handleCloseFile = () =>{
+    setIsModalOpen(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Upload PDF</h1>
+      <input type='file' onChange={handleChangeFile} />
+      <button onClick={handleViewPdf} >View</button>
+
+      {isModalOpen && (
+        <div className='pop-up'>
+          <button onClick={handleCloseFile}>Close</button>
+          <iframe src={pdfUrl} frameBorder='0' title='pdf-view'/>
+        </div>
+      )}
     </div>
   );
 }
